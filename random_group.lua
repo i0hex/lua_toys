86ef40035aa3419c88b1fa8928c906d0a12396a7
Eric <142797755+i0hex@users.noreply.github.com>
2025-08-04 09:51:33 +0800

    Update random_group.lua

--- random_group.lua
+++ random_group.lua
@@ -1,40 +1,33 @@
--- random_group.lua
+-- random_event.lua
 -- @author eric zhang
--- @date Aug 2, 2025
+-- @date Aug 4, 2025
 
 -- Data table
 local data = {
-	-- event_list:table		Event list
-	-- last_group:number 	Last random group
+	-- event_sequence		Event sequence
+	-- last_group_index		Last group index
 }
 
--- Event group
+-- Event groups
 -- { {start1, end1}, {start2, end2}, ... }
-local event_group = {{1, 3}, {4, 6}, {7, 9}, {10, 12}, {13, 15}, {16, 18}, {19, 21}, {22, 24}}
-
-local math_fmod = math.fmod
-local math_floor = math.floor
-local math_random = math.random
-local str_fmt = string.format
+local event_groups = {{1, 3}, {4, 6}, {7, 9}, {10, 12}, {13, 15}, {16, 18}, {19, 21}, {22, 24}}
 
 -- Random event
--- @param times:number Random times
-local function random_event(times)
-	local length = #event_group
-	if length < 3 then
+-- @param num_event:number Number of random events
+local function gen_random_events(num_events)
+	local group_count = #event_groups
+	if group_count < 3 then
 		-- The group must be at least 3!
 		print("the group length must be at least 3!")
 		return
 	end
 
-	local is_even_length = (math_fmod(length, 2) == 0)
-
-	local offset = math_floor(length / 2)
-	if is_even_length then
+	local offset = math.floor(group_count / 2)
+	if math.fmod(group_count, 2) == 0 then
 		-- When the group length is even, the offset and the random value upper bound become equal.
 		-- However, to prevent the grouping from falling into a cycle, the random value upper bound must exceed the offset.
 		-- Thus, the offset can be adjusted to the largest odd number smaller than the current offset.
-		if math_fmod(offset, 2) == 0 then
+		if math.fmod(offset, 2) == 0 then
 			-- If the currently calculated offset is even,
 			-- subtract 1 to obtain the largest odd number.
 			offset = offset - 1
@@ -42,38 +35,38 @@ local function random_event(times)
 			-- If it is odd, subtract 2 to obtain the largest odd number.
 			offset = offset - 2
 		end
+
+		-- offset = math.fmod(offset, 2) == 0 and offset - 1 or offset - 2
 	end
 	-- Random upper bound
-	local rand_max = length - offset
-	print(str_fmt("length:%-3d rand_max:%-3d offset:%-3d", length, rand_max, offset))
+	local max_start_index = group_count - offset
+	print(string.format("count:%-3d max_start_index:%-3d offset:%-3d", group_count, max_start_index, offset))
 
-	for index = 1, times do
-		local start = 0
-		if not data.event_list or math_fmod(#data.event_list, length) == 0 then
+	for index = 1, num_events do
+		if not data.event_sequence or math.fmod(#data.event_sequence, group_count) == 0 then
 			-- Generate the grouping random value randomly at the first occurrence
 			-- and once after every 'length' grouping operations.
-			math.randomseed(os.time() + (data.event_list and #data.event_list or 0))
-			start = math_random(rand_max)
-		elseif data.last_group then
-			-- At all other times, simply use the previously saved group
-			start = data.last_group
+			local seed = os.time()
+			if data.event_sequence then seed = seed + #data.event_sequence end
+			math.randomseed(seed)
+			data.last_group_index = math.random(max_start_index)
 		end
 
 		-- Calculate the current grouping.
-		data.last_group = start + offset
-		if data.last_group > length then
-			-- When the value exceeds the group length, take the modulus with the group length
-			-- to ensure the calculated group falls within the valid range.
-			data.last_group = math_fmod(data.last_group, length)
-		end
+		local current_group_index = math.fmod(data.last_group_index + offset, group_count)
+		if current_group_index == 0 then current_group_index = group_count end
+		data.last_group_index = current_group_index
 
 		-- Randomly select an event from the group
-		local event_id = math_random(event_group[data.last_group][1], event_group[data.last_group][2])
-		if not data.event_list then data.event_list = {} end
-		data.event_list[#data.event_list+1] = { id = event_id }
-		print(str_fmt("index:%-3d start:%-3d last_group:%-3d event_id:%-3d", index, start, data.last_group, event_id))
+		local event_id = math.random(event_groups[current_group_index][1], event_groups[current_group_index][2])
+		if not data.event_sequence then data.event_sequence = {} end
+		data.event_sequence[#data.event_sequence + 1] = { id = event_id }
+		print(string.format("event#%-3d group:%-3d id:%-3d", index, current_group_index, event_id))
 	end
 end
 
-random_event(#event_group * 2)
+gen_random_events(#event_groups * 2)
 
+for index, event in ipairs(data.event_sequence) do
+	print(string.format("index:%-3d id:%-3d", index, event.id))
+end
